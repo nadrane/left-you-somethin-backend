ee7e3677758ad2d97b1556c4f906cf85004f54b9
Alexis J <alexis.m.jenn@gmail.com>
2017-01-25 14:05:44 -0600

    testing

--- src/server/api.js
+++ src/server/api.js
@@ -1,11 +1,8 @@
 const api = require('express').Router();
 
 api
-<<<<<<< HEAD
   .use('/user', require('./user'))
-=======
-  .use('/users', require('./users'))
->>>>>>> 3fbf0ff63ef37bc0c4a8fca66d204d47a8c12111
+
   .use('/messages', require('./messages'))
 
 // Send along any errors
@@ -14,11 +11,7 @@ api.use((err, req, res, next) => {
   res.status(500).send(err);
 });
 
-// No routes matched? 404.
-<<<<<<< HEAD
 api.use((req, res) => res.status(404).end());
 
 module.exports = api
-=======
-api.use((req, res) => res.status(404).end());
->>>>>>> 3fbf0ff63ef37bc0c4a8fca66d204d47a8c12111
+
