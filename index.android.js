1302c5cd86759ed16bfe116f74be36eda09acd50
Jean Chung <jxchung@gmail.com>
2017-01-26 10:59:08 -0600

    this repo now contains app backend only

--- index.android.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-
-import App from './src/App';
-
-AppRegistry.registerComponent('LeftYouSomethin', () => App);
--- index.ios.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-
-import App from './src/App.js';
-
-AppRegistry.registerComponent('LeftYouSomethin', () => App);
