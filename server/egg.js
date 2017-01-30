36ffde9cd844c4eea13eb71586a42f02efe1f9e7
acenorth <north.andy@gmail.com>
2017-01-30 15:00:29 -0600

    tunnels

--- server/egg.js
+++ server/egg.js
@@ -1,23 +1,25 @@
 const { Egg } = require('../db/index.js');
 const router = require('express').Router();
 
-  router.get('/', (req, res, next) => {
-    res.send('Hit Eggs Page')
-  });
 
-  router.get('/:id', (req, res, next) => {
-    Egg.findOne({where: {id: req.params.id}})
+  router.get('/:eggId', (req, res, next) => {
+    Egg.findOne({where: {id: req.params.eggId}})
     .then(egg => res.send(egg))
   });
 
-    router.post('/', (req, res, next) => {
-        Egg.create({
-          goHereText: req.body.goHereText,
-          payloadType: req.body.payloadType,
-          latitude: req.body.latitude,
-          longitude: req.body.longitude
-        })
-            .then(egg => res.send(egg));
-    });
+  router.get('/user/:userId', (req, res, next) => {
+    Egg.findAll({ where: { receiverId: req.params.userId }})
+    .then(eggs => res.send(eggs));
+  });
+
+  router.post('/', (req, res, next) => {
+      Egg.create({
+        goHereText: req.body.goHereText,
+        payloadType: req.body.payloadType,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude
+      })
+          .then(egg => res.send(egg));
+  });
 
 module.exports = router;
